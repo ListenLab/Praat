3e23ffee61577b42e327fd0c5308ab1d07f5418d
Matt Winn <mwinn83@gmail.com>
2020-03-28 15:00:15 -0500

    Add files via upload

---
+++ draw_praat_spectrogram_demo.R
@@ -0,0 +1,291 @@
+library(ggplot2)
+library(dplyr)
+
+rm(list = ls())
+# read the Praat spectrogram functions
+setwd("L:\\Tools\\R\\Read_praat_spectrograms")
+source("praat_spectrogram_functions.R")
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+# here's where to find the files
+spectrogram_file_path <- "C:\\Users\\Matt\\Documents\\R\\Read_praat_spectrograms"
+setwd(spectrogram_file_path)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+my_spect_file <- "lake.Spectrogram"
+
+df_spectrogram <- my_spect_file %>%
+  convert_spectrogram_to_df() %>%
+  pre_emphasize() %>% # creates Column `Level_preemp`
+  constrain_dynamic_range(column = "Level_preemp",
+                          dynamic_range = 95)
+
+summary(df_spectrogram$Level)
+summary(df_spectrogram$Level_preemp)
+summary(df_spectrogram$Level_preemp_dr)
+
+
+
+
+
+
+names(df_spectrogram)
+
+px_spectrogram <- ggplot(df_spectrogram)+
+  aes(x = Time, y=Frequency, fill=Level_preemp_dr)+
+  geom_tile()+
+  scale_fill_gradient(high="black", low="white", na.value = "white")+
+  coord_cartesian(ylim=c(0, 5000), 
+                  xlim = c(0, 0.56),
+                  expand = FALSE)+
+  xlab("Time (s)")+
+  scale_y_continuous("Frequency (Hz)", breaks=seq(0, 5000, 1000))+
+  theme_bw()+
+  theme(legend.position="none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+px_spectrogram
+
+#
+
+
+
+
+
+#======================================#
+# Plot each segment in a different color
+onset_l <- 0.0324
+onset_ei <- 0.1254
+onset_k <- 0.306
+
+df_spectrogram$segment <- 
+  ifelse(df_spectrogram$Time > onset_k, "k",
+         ifelse(df_spectrogram$Time > onset_ei, "eI",
+                ifelse(df_spectrogram$Time > onset_l, "l", NA)))
+
+df_spectrogram$segment <- 
+  factor(df_spectrogram$segment, 
+         levels = c("l","eI","k"))
+
+
+segment_colors <- c("black",
+                    "#8C3B3B",
+                    "#1D2F50")
+
+px_spectrogram_segments <- 
+  df_spectrogram %>%
+  dplyr::filter(!is.na(Level_preemp_dr)) %>%
+  ggplot()+
+  aes(x = Time, y=Frequency, fill=segment,
+      alpha = Level_preemp_dr)+
+  geom_tile()+
+  scale_fill_manual(values = segment_colors)+
+  # scale_alpha_continuous()
+  coord_cartesian(ylim=c(0, 5000), 
+                  xlim = c(0, 0.56),
+                  expand = FALSE)+
+  xlab("Time (s)")+
+  scale_y_continuous("Frequency (Hz)", breaks=seq(0, 5000, 1000))+
+  theme_bw()+
+  theme(legend.position="none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+px_spectrogram_segments
+
+#==========================================#
+# Now with log-scaled Y axis
+octave_breaks <- c(125, 250, 500, 1000, 2000, 4000)
+
+px_spectrogram_log_y <- ggplot(df_spectrogram)+
+  aes(xmin = Time, xmax = Time_next,
+      ymin=Frequency, ymax = Frequency_next,
+      fill=Level_preemp_dr)+
+  geom_rect()+
+  scale_fill_gradient(high="black", low="white", na.value = "white")+
+  coord_cartesian(ylim=c(50, 8000), expand = FALSE)+
+  xlab("Time (s)")+
+  scale_y_log10("Frequency (Hz)", 
+                breaks=octave_breaks)+
+  theme_bw()+
+  theme(legend.position="none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+px_spectrogram_log_y
+
+#=============================================#
+# Plot narrowband style
+my_spect_file_nb <- "lake_narrowband.Spectrogram"
+
+df_spectrogram_nb <- my_spect_file_nb %>%
+  convert_spectrogram_to_df() %>%
+  pre_emphasize() %>%
+  constrain_dynamic_range(column = "Level_preemp",
+                          upper_limit = NULL, 
+                          dynamic_range = 95)
+
+px_spectrogram_log_y_nb <- ggplot(df_spectrogram_nb)+
+  aes(xmin = Time, xmax = Time_next,
+      ymin=Frequency, ymax = Frequency_next,
+      fill=Level_preemp_dr)+
+  geom_rect()+
+  scale_fill_gradient(high="black", low="white", na.value = "white")+
+  coord_cartesian(ylim=c(50, 8000), expand = FALSE)+
+  xlab("Time (s)")+
+  scale_y_log10("Frequency (Hz)", 
+                breaks=octave_breaks)+
+  theme_bw()+
+  theme(legend.position="none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+px_spectrogram_log_y_nb
+
+
+
+
+
+
+# Save some images
+ggsave(px_spectrogram,
+       file = "px_spectrogram.png",
+       height = 3.1, width = 5.5,
+       dpi = 300)
+
+ggsave(px_spectrogram_segments,
+       file = "px_spectrogram_segments.png",
+       height = 3.1, width = 5.5,
+       dpi = 300)
+
+ggsave(px_spectrogram_log_y,
+       file = "px_spectrogram_log_y.png",
+       height = 3.1, width = 5.5,
+       dpi = 300)
+
+ggsave(px_spectrogram_log_y_nb,
+       file = "px_spectrogram_log_y_nb.png",
+       height = 3.1, width = 5.5,
+       dpi = 300)
+
+
+ggsave(px_spectrogram,
+       file = "px_spectrogram.pdf",
+       height = 3.1, width = 5.5,
+       device = cairo_pdf)
+
+
+
+
+
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+# plot "Mongoose"
+
+df_mongoose <- "mongoose_12.Spectrogram" %>%
+  convert_spectrogram_to_df() %>%
+  pre_emphasize() %>%
+  constrain_dynamic_range(column = "Level_preemp", 
+                          dynamic_range = 60)
+
+#-----------------------------------------#
+# Plot each segment in a different color
+onset_m <- 0.0517
+onset_a <- 0.13
+onset_ng <- 0.2535
+onset_g <- 0.3575
+onset_u <- 0.4016
+onset_s <- 0.579
+endpoint <- 0.883
+
+segment_times <-
+  c(
+    onset_m, onset_a, onset_ng, 
+    onset_g, onset_u, onset_s,
+    endpoint)
+
+#-----------------------------------------#
+# Key in the index of the segment
+# at each timepoint
+df_mongoose$segment <- 
+  findInterval(df_mongoose$Time, segment_times)
+
+
+px_mongoose_segments <- 
+  df_mongoose %>%
+  dplyr::filter(!is.na(Level_preemp_dr)) %>%
+  dplyr::filter(segment >= 1) %>%
+  ggplot()+
+  aes(x = Time, y=Frequency, fill=as.factor(segment),
+      alpha = Level_preemp_dr)+
+  geom_tile()+
+  coord_cartesian(ylim=c(0, 6000), 
+                  xlim = c(0, 0.89),
+                  expand = FALSE)+
+  xlab("Time (s)")+
+  scale_y_continuous("Frequency (Hz)", 
+                     breaks=seq(0, 6000, 1000))+
+  theme_bw()+
+  theme(legend.position="none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+px_mongoose_segments
+
+
+# Use a Brewer palette
+px_mongoose_segments_d2 <- 
+  px_mongoose_segments +
+  scale_fill_brewer(palette = "Dark2")
+px_mongoose_segments_d2
+
+
+custom_mongoose_colors <- 
+  c("#e41a1c",
+    "#377eb8",
+    "#4daf4a",
+    "#984ea3",
+    "#ff7f00",
+    "#2E2E2E")
+
+px_mongoose_segments_custom <- 
+  px_mongoose_segments +
+  scale_fill_manual(values = custom_mongoose_colors)
+px_mongoose_segments_custom
+
+ggsave(px_mongoose_segments_custom,
+       file = "px_mongoose_segments.png",
+       height = 3, width = 6, dpi = 300)
+
+
+
+
+
+
+#-----------------------------------#
+px_mongoose_segments_bw <- 
+  df_mongoose %>%
+  dplyr::filter(!is.na(Level_preemp_dr)) %>%
+  dplyr::filter(segment >= 1) %>%
+  ggplot()+
+  aes(x = Time, y=Frequency,
+      alpha = Level_preemp_dr)+
+  geom_tile(fill = "#2E2E2E")+
+  coord_cartesian(ylim=c(0, 6000), 
+                  xlim = c(0, 0.89),
+                  expand = FALSE)+
+  xlab("Time (s)")+
+  scale_y_continuous("Frequency (Hz)", 
+                     breaks=seq(0, 6000, 1000))+
+  theme_bw()+
+  theme(legend.position="none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+px_mongoose_segments_bw
+
+ggsave(px_mongoose_segments_bw,
+       file = "px_mongoose_bw.png",
+       height = 3, width = 6, dpi = 300)
---
+++ praat_spectrogram_functions.R
@@ -0,0 +1,124 @@
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+
+# Note: this sript assumes that you have saved a Praat Spectrogram 
+# using the function "Save to text file. "
+# It will not work if you save to a 'short text file',
+# and will not work if you save to a 'binary file' 
+
+# Here's the main function: 
+convert_spectrogram_to_df <- function(spect_name){
+  # e.g. spect_name = "Time.Spectrogram"
+  
+  # Read the text file
+  spect <- readLines(spect_name) %>% 
+    colsplit(., pattern = " = ", names = c("coordinate","Value")) 
+  
+  # Skip the first three rows
+  spect <- spect[4:nrow(spect),] %>%
+    mutate(Value = as.numeric(Value))
+  
+  # extract info from the file
+  Freq_floor <- spect[spect$coordinate=="ymin", "Value"]
+  Freq_max <- spect[spect$coordinate=="ymax", "Value"]
+  Time_start <- spect[spect$coordinate=="xmin", "Value"]
+  Time_end <- spect[spect$coordinate=="xmax", "Value"]
+  num_times <- spect[spect$coordinate=="nx", "Value"]
+  time_bin_size <- spect[spect$coordinate=="dx", "Value"]
+  num_freq_bins <- spect[spect$coordinate=="ny", "Value"]
+  freq_bin_size <- spect[spect$coordinate=="dy", "Value"]
+  
+  # Get the actual intensity values here
+  spect_vals <- spect$coordinate[12:length(spect$coordinate)] %>% 
+    # thsoe values are loaded into the variable 'meas'
+    data.frame(meas = .) %>%
+    # convert to character
+    mutate(meas = as.character(meas)) %>%
+    # look for :  - if yes, then mark it as a frequency step
+    mutate(is.Freqstep = ifelse(grepl(x = meas, pattern=":"), 1, 0)) %>%
+    # tally the number of frequency steps
+    mutate(Freqstep = cumsum(is.Freqstep)) %>%
+    # calculate the time step
+    mutate(Timestep = {meas %>% 
+        colsplit(., pattern = "\\]|\\]", names = c("junk","Timebin")) %>%
+        mutate(Timebin = gsub(replacement = "", x = Timebin, pattern="\\[|\\]")) %>%
+        # encode the time bin as an index
+        mutate(Timebin = as.numeric(Timebin)) %>%
+        `[[`(., "Timebin")}) %>%
+    #-------------------------------------------------------#
+    # declare the actual time value
+    # by multiplying the index by the size of the time bin
+    mutate(Time = (Timestep - 1) * time_bin_size) %>%
+    # get the value for the *next* timebin
+    # for drawing tiles
+    mutate(Time_next = (Timestep) * time_bin_size) %>%
+    #-------------------------------------------------------#
+    # do the same for the frequency value
+    mutate(Frequency = (Freqstep - 1) * freq_bin_size) %>%
+    # get teh value for the *next* frequency bin
+    mutate(Frequency_next = (Freqstep) * freq_bin_size) 
+  
+  # This is the reference value for 0 dB
+  reference_SP <- 0.00002
+  
+  # Build the output data frame
+  spectrogram <- cbind(spect[12:nrow(spect),], spect_vals) %>%
+    mutate(filename = spect_name) %>%
+    select(filename, Timestep, Freqstep, 
+           Time, Time_next, 
+           Frequency, Frequency_next, 
+           Pressure = Value) %>%
+    # Convert sound pressure to dB scale
+    mutate(Level = 20 * log10(Pressure / reference_SP))
+  
+  # Get rid of NA values (don't know why this is necessary, but it is)
+  spectrogram <- spectrogram[!is.na(spectrogram$Time),]
+  
+  rm(spect, spect_vals)
+  return(spectrogram)
+}
+
+constrain_dynamic_range <- function(spectrogram_object, 
+                                    column = "Level", 
+                                    upper_limit = 91,
+                                    dynamic_range = 60){
+  # Here's how to constrain dynamic range
+  # with reference to the highest value
+  # (the way you'd do in Praat)
+  
+  # First make a copy of the Level variable,
+  # so that you don't lose information
+  intensity_values <- 
+    spectrogram_object[column]
+
+  if(is.null(upper_limit)){
+    upper_limit <- max(intensity_values, na.rm=TRUE)
+  }
+  
+  # truncate the upper limit
+  intensity_values[intensity_values > upper_limit] <- upper_limit
+  
+  
+  # Remove anything that's more than [DR] below the uppr limit
+  intensity_values[
+    intensity_values < (upper_limit - dynamic_range)] <- NA
+  
+  spectrogram_object[paste0(column, "_dr")] <- intensity_values
+  return(spectrogram_object)
+}
+
+pre_emphasize <- function(spectrogram_object, column = "Level", starting_freq = 50){
+  
+  # calculate the number of octaves away 
+  # from the starting frequency for pre-emphasis
+  spectrogram_object$octaves_from_ref <- 
+    log2(spectrogram_object$Frequency / starting_freq)
+  
+  # for each octave, add 6 dB. 
+  # use the column declared in the input arguments
+  spectrogram_object[paste0(column, "_preemp")] <- 
+    spectrogram_object[column] + (6 * spectrogram_object$octaves_from_ref)
+  
+  return(spectrogram_object)
+}
